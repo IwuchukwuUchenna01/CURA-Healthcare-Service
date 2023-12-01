7b0c0b1395f7613e80cd06583a9e45966ad9c84a
uchenna Iwuchukwu <oreoluwa371@gmail.com>
2023-12-01 13:36:07 +0100

    black Box further Testing

--- src/test/java/org/example/CuraHealthTest.java
+++ src/test/java/org/example/CuraHealthTest.java
@@ -14,12 +14,30 @@ public class CuraHealthTest {
     }
     @Test(priority = 0)
     @Description("Appointment Confirmation\n" +
-            "Please be informed that your appointment has been booked as following:")
+            "Please be informed that your appointment has been booked as following:\n"+
+    "Facility:  " + "\t\t\tTokyo CURA Healthcare Center\n" +
+            "\n" +
+            "Apply for hospital readmission:    " +
+            "\t\t\tYes" +
+            "\n" +
+            "Healthcare Program:     " +
+            "\t\t\tMedicaid" +
+            "\n" +
+            "Visit Date:     " +
+            "\t\t\t20/12/2023" +
+            "\n" +
+            "Comment:    " +
+            "\t\t\tthis is the date suitable for me")
     public void bookAppointment(){
         cure.makeAppointment();
     }
-    @AfterTest
+    @Test(priority = 1)
+    @Description("Logs User out")
     public void logout(){
         cure.logout();
+    }
+    @AfterTest
+    public void closeUp(){
+        cure.dissolve();
     }
 }
